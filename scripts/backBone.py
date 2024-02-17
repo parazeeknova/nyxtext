f637e5a6f6bf1e00d4eb4585d95f7c4f47b3bf70
Sam2503 <samarthsin2006@gmail.com>
2024-02-18 01:16:22 +0530

    Update backBone.py

--- scripts/backBone.py
+++ scripts/backBone.py
@@ -81,6 +81,10 @@ def open_new_window():
 root = tk.Tk()
 root.title("Python Text Editor")
 
+#Logo
+logo = tk.PhotoImage(file="main\\assets\\logo\\text-edit-icon-9.png")
+root.iconphoto(False, logo)
+
 # Create a text area
 text_area = tk.Text(root, wrap=tk.WORD)
 text_area.pack(expand=True, fill=tk.BOTH)
