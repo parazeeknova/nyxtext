ad04dc4b13920d166971168eea78afa63bffa16b
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-03-01 21:40:41 +0530

    Fixed Intention

--- editor/scripts/settings.py
+++ editor/scripts/settings.py
@@ -9,10 +9,10 @@ from text_Area import textarea
 class Settings():
    
    def __init__(self, master):
-        self.settings_window = customtkinter.CTkToplevel(master)
-        self.settings_window.title("Settings")
-        self.settings_window.geometry("300x200")
-        customtkinter.CTkLabel(self.settings_window, text="Settings Window").pack()
+         self.settings_window = customtkinter.CTkToplevel(master)
+         self.settings_window.title("Settings")
+         self.settings_window.geometry("300x200")
+         customtkinter.CTkLabel(self.settings_window, text="Settings Window").pack()
 
 
    
