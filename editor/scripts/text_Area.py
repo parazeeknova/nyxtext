29ae478d84fc0434587757f91250f48cb641b0ef
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-03-05 22:32:02 +0530

    Removed unwanted function for incoming feature update

--- editor/scripts/text_Area.py
+++ editor/scripts/text_Area.py
@@ -9,9 +9,3 @@ class textarea():
         self.text_area = ctk.CTkTextbox(parent_frame, height= screen_height-20, width = rf, activate_scrollbars = True, wrap = 'none')
         self.text_area.grid(row = 0, column = 1, sticky = 'nsew')
         self.text_area.configure(padx = 10, pady = 10,takefocus = True)
-        # Set default font
-        
-    def set_font(self, font_name):
-        font = ctk.CTkFont(family=font_name, size=12)
-        self.text_area.configure(font=font)
-        self.text_area.update()
