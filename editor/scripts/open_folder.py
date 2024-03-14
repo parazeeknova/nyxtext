e1c6fb20d8b6e7c375cf465c27f1af4e95b46b5b
Rishabh Pandey <rpandey126.jpsv@gmail.com>
2024-03-14 15:19:29 +0530

    open_folder  completed

--- editor/scripts/open_folder.py
+++ editor/scripts/open_folder.py
@@ -1,13 +1,49 @@
 import customtkinter as ctk
-
+from tkinter import filedialog
+import tkinter as tk
 class openfolder_window(ctk.CTkToplevel):
     def __init__(self, master=None):
         super().__init__(master)
         self.geometry("400x200")
-        self.title("New File")
+        self.title("New folder")
+        self.wm_overrideredirect(True)
         
-        # Create a label for New file title
-        self.openfolder_label = ctk.CTkLabel(self, text="Open Folder content in workspaces :",
-                                                font=('Calibri',18,"bold"),
-                                                padx=100,anchor="center")
-        self.openfolder_label.pack(side='top',pady=(20,0))
+        # Center the window on the screen
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        window_width = 280
+        window_height = 250
+        position_top = int(screen_height / 2 - window_height / 2)
+        position_right = int(screen_width / 2 - window_width / 2)
+        self.geometry(f"{window_width}x{window_height}+{position_right}+{position_top}")
+        self.attributes('-alpha', 0.9)
+        
+        self.close_button = ctk.CTkButton(self, text="X", command=self.destroy, width=6, height=6, fg_color='transparent', text_color='#ed8796')
+        self.close_button.place(x=24, y=1)
+        # Create a label for New folder title
+        self.openfolder_label = ctk.CTkLabel(self, text="Folder Options :",font=("JetBrainsMono NF", 19, "bold"), padx=100,)
+        self.openfolder_label.pack(side='top', pady=(20,0))
+        
+        self.button_open = ctk.CTkButton(self, text="Open Folder", width=100, height=25, corner_radius=10, command=self.open_folder)
+        self.button_open.pack(side="top",pady=5)
+        # Buttons
+        self.button1 = ctk.CTkButton(self, text="New folder in Workspace", width=100, height=25, corner_radius=10)
+        self.button1.pack(side='top', pady=5)
+        
+        self.button2 = ctk.CTkButton(self, text="New folder in Codespace", width=100, height=25, corner_radius=10)
+        self.button2.pack(side='top',pady=5)
+        
+        self.button3 = ctk.CTkButton(self, text="Open folder in Workspace", width=100, height=25, corner_radius=10)
+        self.button3.pack(side='top', pady=5)
+        
+        self.button4 = ctk.CTkButton(self, text="Open folder in Codespace", width=100, height=25, corner_radius=10)
+        self.button4.pack(side='top',pady=5)
+        
+    def open_folder(self):
+        self.Folder_path_1 = filedialog.askopenFoldername(defaultextension=".txt",
+                                        Foldertypes=[("Text Folder", "*.txt"), ("All Folders", "*.*")])
+        if self.Folder_path_1:
+            with open(self.Folder_path_1, "r") as Folder:
+                self.text_Area.text_area.delete(1.0, tk.END)  # Clear the text area
+                self.text_Area.text_area.insert(tk.INSERT)
+
